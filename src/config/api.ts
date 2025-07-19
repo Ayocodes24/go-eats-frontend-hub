5ec7a5a624a30a46dadc6644be620c7f34bb96d4
Ayush Saxena <153740193+Ayocodes24@users.noreply.github.com>
2025-07-19 12:37:12 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,6 @@
 // Replace with your actual backend URL and port
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+
 
 
 export const API_ENDPOINTS = {
